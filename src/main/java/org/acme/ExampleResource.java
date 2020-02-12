f8114099419905936ee15495586a63805908db97
Sergey Shvyrev <sergey@MacBook-Pro-Sergey.local>
2020-02-12 08:12:57 +0300

    add two version of people GET request for two different exception demonstration

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,17 +1,29 @@
 package org.acme;
 
+import io.quarkus.arc.ManagedContext;
 import io.vertx.axle.core.Vertx;
+import io.vertx.axle.core.buffer.Buffer;
+import io.vertx.axle.ext.web.client.HttpResponse;
+import io.vertx.axle.ext.web.client.WebClient;
+import io.vertx.core.json.JsonObject;
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.context.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.jvm.hotspot.debugger.ThreadContext;
+import io.vertx.ext.web.client.WebClientOptions;
 
 import javax.enterprise.context.control.ActivateRequestContext;
 import javax.inject.Inject;
+import javax.transaction.SystemException;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
 @ActivateRequestContext
 @Path("/hello")
@@ -23,6 +35,9 @@ public class ExampleResource {
     @Inject
     ThreadContext threadContext;
 
+    @Inject
+    ManagedExecutor managedExecutor;
+
     private static final Logger log = LoggerFactory.getLogger( ExampleResource.class );
 
     @Transactional
@@ -32,5 +47,60 @@ public class ExampleResource {
         final Person person = new Person();
         person.persist();
         return "hello";
+    }
+
+    @Transactional
+    @GET
+    @Path("/v1/people")
+    public CompletionStage<List<Person>> firstVersionPeople() throws SystemException {
+        // Create a REST client to the Star Wars API
+        WebClient client = WebClient.create(vertx,
+                new WebClientOptions()
+                        .setDefaultHost("swapi.co")
+                        .setDefaultPort(443)
+                        .setSsl(true));
+
+        // get the list of Star Wars people, with context capture
+        return threadContext.withContextCapture(client.get("/api/people/").send())
+                .thenApply(response -> {
+                    JsonObject json = response.bodyAsJsonObject();
+                    List<Person> persons = new ArrayList<>(json.getInteger("count"));
+                    // Store them in the DB
+                    // Note that we're still in the same transaction as the outer method
+                    for (Object element : json.getJsonArray("results")) {
+                        Person person = new Person();
+                        person.name = ((JsonObject) element).getString("name");
+                        person.persist();
+                        persons.add(person);
+                    }
+                    return persons;
+                });
+    }
+
+
+    @Transactional
+    @GET
+    @Path("/v2/people")
+    public CompletionStage<List<Person>> secondVersionPeople() throws SystemException {
+        // Create a REST client to the Star Wars API
+        WebClient client = WebClient.create(vertx,
+                new WebClientOptions()
+                        .setDefaultHost("swapi.co")
+                        .setDefaultPort(443)
+                        .setSsl(true));
+        return client.get("/api/people").send()
+                .thenCompose(response -> managedExecutor.supplyAsync(() -> {
+                    final JsonObject json = response.bodyAsJsonObject();
+                    List<Person> persons = new ArrayList<>(json.getInteger("count"));
+                    // Store them in the DB
+                    // Note that we're still in the same transaction as the outer method
+                    for (Object element : json.getJsonArray("results")) {
+                        Person person = new Person();
+                        person.name = ((JsonObject) element).getString("name");
+                        person.persist();
+                        persons.add(person);
+                    }
+                    return persons;
+                }));
     }
 }
